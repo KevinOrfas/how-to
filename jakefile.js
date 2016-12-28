e59aa3bc3c89cdb39aabe04c93e0fdf63b07a75e
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2016-12-28 13:18:16 +0000

    Created reproducible build

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,7 @@
 (function(){
     'use strict';
-    
+
+    var semver = require('semver');
     
 
     desc('Default build');
@@ -15,7 +16,7 @@
         let packageJson = require('./package.json');
         let expectedVersion = 'v' + packageJson.engines.node;
         let actualVersion = process.version;
-        if(actualVersion !== expectedVersion) {
+        if(semver.neq(expectedVersion, actualVersion)) {
             fail('Incorrect Node version does not match ' + expectedVersion + ', but was ' +actualVersion);
         }
         console.log(process.version);
