4e5b4cc3fc5c789f32f7cc7bd245c88e9e1efb3c
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2016-12-28 12:42:32 +0000

    Chech node version

--- jakefile.js
+++ jakefile.js
@@ -1,10 +1,22 @@
 (function(){
     'use strict';
     
+    const EXPECTED_NODE_VERSION = 'v4.2.6';
 
     desc('Default build');
-    task('default', function(){
+    task('default', ['version'], function(){
         console.log('\n\nBuild OK');
+    });
+
+    desc('Check NodeJS version');
+    task('version', function(){
+        console.log('Checking NodeJS version: .');
+
+        const actualVersion = process.version;
+        if(actualVersion !== EXPECTED_NODE_VERSION) {
+            fail('Incorrect Node version does not match ' + EXPECTED_NODE_VERSION + ', but was ' +actualVersion);
+        }
+        console.log(process.version);
     });
 
 })();
