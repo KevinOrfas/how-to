c7a388b2b3df6f5d50fa54332ecb84f67cd8e887
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2016-12-29 11:59:26 +0000

    Set up mocha

--- src/test.js
+++ src/test.js
@@ -6,11 +6,13 @@
         return a + b;
     }
 
-    //basic addition
-    assert.equal(add(3,4), 7);
-
-    //IEEE 754 floating point
-    assert.equal(add(0.1,0.2), 0.30000000000000004);
-
+    describe('Addition', function(){
+        it('adds positive numbers', function(){
+            assert.equal(add(3,4), 7);
+        })
+        it('uses IEEE 754 floating point', function(){
+            assert.equal(add(0.1,0.2), 0.30000000000000004);
+        })
+    });
     
 })();
