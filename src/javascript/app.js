ac297425102aba608d1933a74209a86cab4fb3dc
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2016-12-30 15:21:36 +0000

    Adde styles and html and rename test and erase some test

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,9 +1,7 @@
 (function(){
     'use strict';
     
-    var arithmetic = require('./arithmetic.js');
     
-    console.log('Hello');
-    console.log('1 + 1 = ' + arithmetic.add(1,1));
+    
 
 })();
