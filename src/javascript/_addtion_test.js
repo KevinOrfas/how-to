d941448bb51a1b366b500d45760769bad79052c7
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2016-12-30 13:19:24 +0000

    Add chai to karma config

--- src/javascript/_addtion_test.js
+++ src/javascript/_addtion_test.js
@@ -1,14 +1,14 @@
 (function(){
     'use strict';
-    // var assert = require('chai').assert;
+    var assert = require('../vendor/chai-4.0.0.js').assert;
     var addition = require('./addition.js');
 
     describe('Addition', function(){
         it('adds positive numbers', function(){
-            // assert.equal(addition.add(3,4), 7);
+            assert.equal(addition.add(3,4), 7);
         });
         it('uses IEEE 754 floating point', function(){
-            // assert.equal(addition.add(0.1,0.2), 0.30000000000000004);
+            assert.equal(addition.add(0.1,0.2), 0.30000000000000004);
         });
     });
     
