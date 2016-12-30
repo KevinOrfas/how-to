91e07956123c790d2d6ef980139cb8db2dacfc61
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2016-12-30 17:02:49 +0000

    Create addlement function

--- src/javascript/_test.js
+++ src/javascript/_test.js
@@ -6,12 +6,29 @@
 
     describe('Tabs', function() {
         it('hiddes an elament', function() {
-
+            //arrange 
+            //create the lement
+            var element = addElement('div');
+            //act
             tabs.initialize(element);
 
+            //assert element is not vivisble
+            var styles =  getComputedStyle(element);
+            var display = styles.getPropertyValue('display');
+           
+            assert.equal(display, 'none');
+
+            // reset
+            //remove test element
         });
         
     });
+
+    function addElement(tagName){
+        var element = document.createElement(tagName);
+        document.body.appendChild(element);
+        return element;
+    }
 
 
    
--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -1,3 +1,8 @@
-exports.initialize = function initialize(){
+(function(){
+    'use strict';
 
-};
+    exports.initialize = function initialize(element){
+        element.style.display = 'none';
+    };
+
+})();
