f632a716d547203fe7d67ba01ec83f651df687b9
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-03 16:35:26 +0000

    Create a function to find the index of the default tab

--- src/javascript/_test_tabs.js
+++ src/javascript/_test_tabs.js
@@ -56,17 +56,24 @@
 		});
 
         it('styles the default tab  with a class', function(){
+            var tab1 = createTab();
             var defaultTab = createTab();
-            var defaultContent = createTab();
+            var tab3 = createTab();
+
+            var defaultContent = createTabContent();
 
             tabs.initialize({
-                tabs: [ defaultTab ],
-                content: [ defaultContent ],
+                tabs: [tab1, defaultTab, tab3],
+                content: [ createTabContent(), defaultContent, createTabContent() ],
                 default: defaultContent,
                 activeTabClass: 'activeTab',
                 contentHideClass: IRRELEVANT
             });
 
+            assert.equal(getClasses(tab1), null, 'tab1 should not be styled');
+            assert.equal(getClasses(defaultTab), 'activeTab', 'defaultTab should be styled');
+            assert.equal(getClasses(tab3), null, 'tab3 should not be styled');
+            
             assert.equal(getClasses(defaultTab), 'activeTab');
         });
 
