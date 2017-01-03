6bd1e97349765fb5fcc349555d0ad9e0ebb22d61
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-03 15:22:44 +0000

    Add test for tabs

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -5,18 +5,23 @@
     classList.shim();
 
     exports.initialize = function initialize(options) {
+        var tabs = options.tabs;
         var content = options.content;
         var defaultElement = options.default;
         var contentHideClass = options.contentHideClass;
+        var activeTabClass = options.activeTabClass;
 
+        if(tabs === undefined) throw new Error('Expected options.tabs');
         if(content === undefined) throw new Error('Expected options.content');
         if(defaultElement === undefined) throw new Error('Expected options.default');
         if(contentHideClass === undefined) throw new Error('Expected options.contentHideClass');
+        if(activeTabClass === undefined) throw new Error('Expected options.activeTabClass');
 
         content.forEach(function(element){
             element.classList.add(contentHideClass);
         });
         defaultElement.classList.remove(contentHideClass);
+        if (tabs !== undefined) tabs[0].classList.add(activeTabClass);
     };
 
 })();
