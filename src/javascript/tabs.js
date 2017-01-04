a0c51e41db95cb3d425c047a2d9a96f63809c489
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-04 14:20:57 +0000

    Write more helpful error messages

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -41,11 +41,11 @@
         
     }
 
-    function findIndex(contentTabs, defaultContentTab) {
+    function findIndex(contentTabs, tabToShow) {
         for (var i = 0; i < contentTabs.length; i++) {
-            if(contentTabs[i] === defaultContentTab) return i;
+            if(contentTabs[i] === tabToShow) return i;
         }
-        throw new Error('Could not find default in list');
+        throw new Error('Could not find tab to show' + tabToShow.outterHTML);
     }   
 
     function checkOption(option, name) {
