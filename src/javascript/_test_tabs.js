13efd5b8d7363721d544f40563251f6587982e25
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-04 11:39:07 +0000

    Refactor out assertContentHidden

--- src/javascript/_test_tabs.js
+++ src/javascript/_test_tabs.js
@@ -7,6 +7,7 @@
 
     describe('Tabs', function() {
 
+        const HIDDEN_CONTENT = 'hideClass';
         const IRRELEVANT = 'irrelevant';
         var container;
 
@@ -19,8 +20,9 @@
             removeElement(container);
         });
 
-        it('hides all content elements except the default upon initialization', function() {
+        it('use a class to hide all content elements except the default upon initialization', function() {
             var defaultTab = createTab();
+            
             var content1 = createTabContent();
             var defaultContent = createTabContent();
             var content3 = createTabContent();
@@ -30,16 +32,25 @@
                 content: [content1, defaultContent, content3],
                 defaultTab: defaultTab,
                 activeTabClass: IRRELEVANT,
-                hiddenContentClass: 'hideClass'
+                hiddenContentClass: HIDDEN_CONTENT
             });
             
-            // assert that elements are hidden
-            assert.equal(getClasses(content1), 'hideClass', 'element 1 should be hidden');
-            assert.equal(getClasses(defaultContent), '', 'default element should not be hidden');
-            assert.equal(getClasses(content3), 'hideClass', 'element 3 should be hidden');
+          
+            assertContentHidden(content1, 'element1');
+            assertContentVisible(defaultContent, 'default element');
+            assertContentHidden(content3, 'element3');
+           
         });
 
-        it('styles the default tab  with a class', function(){
+        function assertContentHidden(element, elementName) {
+            assert.equal(getClasses(element), HIDDEN_CONTENT, elementName + ' should be hidden');
+        }
+
+        function assertContentVisible(element, elementName) {
+            assert.equal(getClasses(element), '', elementName + ' should not be hidden');
+        }
+
+        it('styles the default tab  with a class upon initialization', function(){
             var tab1 = createTab();
             var defaultTab = createTab();
             var tab3 = createTab();
@@ -59,6 +70,26 @@
             assert.equal(getClasses(tab3), null, 'tab3 should not be styled');
             
             assert.equal(getClasses(defaultTab), 'activeTab');
+        });
+
+        it('switch content when tab is clicked ', function(){
+            var tab1 = createTab();
+            var tab2 = createTab();
+            var tab3 = createTab();
+
+            var content1 = createTabContent();
+            var content2 = createTabContent();
+            var content3 = createTabContent();
+
+            tabs.initialize({
+                tabs: [tab1, tab2, tab3],
+                content: [ content1, content2, content3 ],
+                defaultTab: tab1,
+                activeTabClass: 'activeTab',
+                hiddenContentClass: HIDDEN_CONTENT
+            });
+
+
         });
 
         it('preserves existing classes when adding new classes', function() {
